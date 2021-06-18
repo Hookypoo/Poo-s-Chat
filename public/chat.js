28b69c513c6936301d49f743422a3e5906cfa39d
Hookypoo <forde.hugh@yahoo.com>
2021-06-17 20:43:25 -0400

    added emoji and timestamp feature

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,10 @@
 //Make connection on frontend/client
 const socket = io.connect("http://localhost:4000");
 
+//get time
+const date = new Date();
+const time = date.getHours()+":"+date.getMinutes()+ ":" +date.getSeconds();
+
 //Querying the DOM
 const message = document.getElementById("message");
 const handle = document.getElementById("handle");
@@ -15,10 +19,8 @@ btn.addEventListener("click", function () {
     socket.emit("chat", {
         message: message.value,
         handle: handle.value,        
-    });
-    
+    });    
 });
-
 
 message.addEventListener("keypress", function() {
     socket.emit("typing", handle.value);
@@ -28,7 +30,7 @@ message.addEventListener("keypress", function() {
 
 socket.on("chat", function (data) {
     feedback.innerHTML="";
-    output.innerHTML += "<p><strong>" + data.handle + ":" + " " +"</strong>" + data.message + "</p>";
+    output.innerHTML += "<p><strong>" + data.handle + ":" + " " +"</strong>" + data.message + time + "</p>";
 });
 
 socket.on("typing", function(data){
