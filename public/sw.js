cd4dfa7c580cc6c586d53a72b92ba35e18d124e7
Hookypoo <forde.hugh@yahoo.com>
2021-06-23 22:40:44 -0400

    added service worker active and fetch events

--- public/sw.js
+++ public/sw.js
@@ -2,4 +2,16 @@
 
 self.addEventListener("install", evt => {
     console.log("service worker has been installed");
-})
+});
+
+//listen and use activate event
+
+self.addEventListener("activate", evt => {
+    console.log("service worker has been activated");
+});
+
+//listen for fetch event
+
+self.addEventListener("fetch", evt => {
+    console.log("fetch event", evt);
+});
