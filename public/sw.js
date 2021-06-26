fd93777182e2d04ca8de4c7e0fe92f95849272e0
Hookypoo <forde.hugh@yahoo.com>
2021-06-25 23:18:50 -0400

    Added cache and offline PWA functionality

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,42 @@
 //using install service worker event
 
+const staticCacheName = "site-static";
+const assets = [
+    "/",    
+    "./chat.js",
+    "./index.html",
+    "./styles.css",    
+    "./emojionearea.min.js", 
+    "./emojionearea.min.css", 
+    "https://code.jquery.com/jquery-3.6.0.min.js",   
+    "https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.0/socket.io.js",
+    
+       
+];
+
 self.addEventListener("install", evt => {
-    console.log("service worker has been installed");
+    //console.log("service worker has been installed");
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log("caching shell assets");
+            cache.addAll(assets);
+       })
+    );
 });
 
 //listen and use activate event
 
 self.addEventListener("activate", evt => {
-    console.log("service worker has been activated");
+    //console.log("service worker has been activated");
 });
 
 //listen for fetch event
 
 self.addEventListener("fetch", evt => {
-    console.log("fetch event", evt);
+    //console.log("fetch event", evt);
+    // evt.respondWith(
+    //     caches.match(evt.request).them(cacheRes => {
+    //         return cacheRes || fetch(evt.request);
+    //     })
+    // );
 });
